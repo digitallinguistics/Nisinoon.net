c05390c71a0906086d4c2dab57072cfad994d929
Daniel W. Hieber <dwhieb@gmail.com>
2022-12-10 03:24:23 -0700

    NEW: use curly quotes (#26)
    closes #26

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -1,6 +1,15 @@
+const createMarkdownParser = require(`markdown-it`)
+
+const markdownParser = createMarkdownParser({
+  html       : true,
+  quotes     : `“”‘’`,
+  typographer: true,
+})
+
 module.exports = function(config) {
 
   config.addPassthroughCopy(`src/images`)
+  config.setLibrary(`md`, markdownParser)
 
   return {
     dir: {
