c94241190b76758c1954f7bbb98e8980d2e7b5c5
Daniel W. Hieber <dwhieb@gmail.com>
2022-12-10 12:32:03 -0700

    DEV: set up CSS build step (#28)
    closes #28

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -1,23 +1,39 @@
+/* eslint-env commonjs */
+
 const createMarkdownParser = require(`markdown-it`)
+const less                 = require(`less`)
+
+const lessOptions = {
+  paths: [
+    `src/layouts/main`,
+    `src/pages`,
+  ],
+}
 
 const markdownParser = createMarkdownParser({
-  html       : true,
-  quotes     : `“”‘’`,
+  html:        true,
+  quotes:      `“”‘’`,
   typographer: true,
 })
 
-module.exports = function(config) {
+function convertLESS(input, cb) {
+  less.render(input, lessOptions)
+  .then(({ css }) => cb(null, css))
+}
 
+module.exports = function configureEleventy(config) {
+
+  config.addNunjucksAsyncFilter(`css`, convertLESS)
   config.addPassthroughCopy(`src/images`)
   config.setLibrary(`md`, markdownParser)
 
   return {
     dir: {
-      data    : `data`,
+      data:     `data`,
       includes: `partials`,
-      input   : `src`,
-      layouts : `layouts`,
-      output  : `dist`,
+      input:    `src`,
+      layouts:  `layouts`,
+      output:   `dist`,
     },
     templateFormats: [`md`, `njk`],
   }
