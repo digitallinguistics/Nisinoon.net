70e719ed9bb90b1bb09af8ed2a0144ad55b1966f
Daniel W. Hieber <dwhieb@gmail.com>
2022-12-09 18:50:31 -0700

    DOCS: test for correct year in license (#1)
    closes #1

--- docs.test.js
+++ docs.test.js
@@ -1,5 +1,19 @@
-import { expect } from 'chai'
+import { expect }   from 'chai'
+import { readFile } from 'fs/promises'
 
-describe(`tests`, function() {
-  expect(true).to.be.true
+describe(`LICENSE`, function() {
+
+  it(`should have the correct year`, async function() {
+
+    const license    = await readFile(`LICENSE.md`, `utf8`)
+    const yearRegExp = /2018–(?<year>\d{4})/u
+
+    const { groups: { year } } = yearRegExp.exec(license)
+    const currentYear = new Date().getUTCFullYear()
+
+    expect(year).to.equal(String(currentYear))
+
+  })
+
+
 })
