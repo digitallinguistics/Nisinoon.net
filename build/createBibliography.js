ce6f04be2f1d8543a12a9f99c4b1be83a475bfe7
Daniel W. Hieber <dwhieb@gmail.com>
2022-12-10 02:16:37 -0700

    render bibliography

--- build/createBibliography.js
+++ build/createBibliography.js
@@ -10,33 +10,38 @@ import {
 const { readFile, outputFile } = fs
 
 const __dirname  = getDirname(fileURLToPath(import.meta.url))
-const bibtexPath = joinPath(__dirname, `../bibliography/bibliography.bib`)
-const bibtex     = await readFile(bibtexPath, `utf8`)
 
-const citer = new Citer(bibtex, {
-  forceType:     `@bibtex/text`,
-  generateGraph: false,
-})
+export default async function createBibliography() {
 
-citer.sort([`author`, `issued`, `title`]) // adding "editor" to the sort list throws an error
+  const bibtexPath = joinPath(__dirname, `../bibliography/bibliography.bib`)
+  const bibtex = await readFile(bibtexPath, `utf8`)
 
-function adjustHTML(id, raw) {
-  return raw
-  .trim()
-  .replace(/<div.+?>(.+)<\/div>/u, `<li id='${ id }'>$1</li>`) // replace <div> with <li>
-  .replace(/<i>(.+)<\/i>/u, `<cite class=cite>$1</cite>`)      // replace <i> with <cite>
+  const citer = new Citer(bibtex, {
+    forceType:     `@bibtex/text`,
+    generateGraph: false,
+  })
+
+  citer.sort([`author`, `issued`, `title`]) // adding "editor" to the sort list throws an error
+
+  function adjustHTML(id, raw) {
+    return raw
+    .trim()
+    .replace(/<div.+?>(.+)<\/div>/u, `<li id='${ id }'>$1</li>`) // replace <div> with <li>
+    .replace(/<i>(.+)<\/i>/u, `<cite class=cite>$1</cite>`)      // replace <i> with <cite>
+  }
+
+  const htmlEntries = citer.format(`bibliography`, {
+    asEntryArray: true,
+    format:       `html`,
+    template:     `ling`,
+  })
+  .map(([id, raw]) => adjustHTML(id, raw))
+  .join(`\n`)
+
+  const htmlPath = joinPath(__dirname, `../src/pages/Bibliography/entries.html`)
+
+  const html = `<ul>\n${ htmlEntries }\n</ul>`
+
+  await outputFile(htmlPath, html, `utf8`)
+
 }
-
-const htmlEntries = citer.format(`bibliography`, {
-  asEntryArray: true,
-  format:       `html`,
-  template:     `ling`,
-})
-.map(([id, raw]) => adjustHTML(id, raw))
-.join(`\n`)
-
-const htmlPath = joinPath(__dirname, `../src/partials/bibliography.html`)
-
-const html = `<ul>\n${ htmlEntries }\n</ul>`
-
-await outputFile(htmlPath, html, `utf8`)
